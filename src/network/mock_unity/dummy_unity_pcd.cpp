49f1c2f82b680ccb16ee7e3ff0101ad570ca817b
victoriayuechen <y.chen-72@student.tudelft.nl>
2023-06-06 23:08:54 +0200

    Check if WSL can get to Windows

--- src/network/mock_unity/dummy_unity_pcd.cpp
+++ src/network/mock_unity/dummy_unity_pcd.cpp
@@ -16,14 +16,14 @@ int main() {
     // Server address (Address of PCL)
     sockaddr_in serverAddress{};
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");  // Connect to localhost
-    serverAddress.sin_port = htons(8082);                   // Use the same port as the server
+    serverAddress.sin_addr.s_addr = inet_addr("0.0.0.0");  // Connect to localhost
+    serverAddress.sin_port = htons(8081);                   // Use the same port as the server
 
     // Bind the socket to a specific address and port (Address of Unity)
     sockaddr_in unityAddress{};
     unityAddress.sin_family = AF_INET;
-    unityAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); // Bind to any available network interface
-    unityAddress.sin_port = htons(8083);
+    unityAddress.sin_addr.s_addr = inet_addr("172.26.165.43"); // Bind to any available network interface
+    unityAddress.sin_port = htons(8080);
 
     if (bind(unitySocket, (struct sockaddr*)&unityAddress, sizeof(unityAddress)) == -1) {
         std::cout << "Failed to bind socket." << std::endl;
@@ -38,9 +38,11 @@ int main() {
                              std::istreambuf_iterator<char>());
     auto cloudData = inputContents.c_str();
 
+    auto message = "hello from pcl";
+
     // Keep sending frames
     while (true) {
-        if ((sendto(unitySocket, cloudData, strlen(cloudData), 0, (struct sockaddr*)&serverAddress, serverAddressLength)) <= 0) {
+        if ((sendto(unitySocket, message, strlen(message), 0, (struct sockaddr*)&serverAddress, serverAddressLength)) <= 0) {
             std::cout << "Couldn't send to PCL tracking program! " << std::endl;
             break;
         }
