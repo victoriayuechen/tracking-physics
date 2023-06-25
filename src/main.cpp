b9dba573404ce421ba9022b29da1d669523ae67e
victoriayuechen <y.chen-72@student.tudelft.nl>
2023-06-25 07:35:52 +0200

    Pareto frontiner experiment data

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <chrono>
 #include "tracker/pcl_tracking.hpp"
 
-int time_experiments(int numParticles, int count) {
+int time_experiments(int numParticles, int res, int count, int run) {
+    double variance = 0.005; 
+    if (res == 2) {
+        variance = 0.008; 
+    }
     // Parameters for the filter
     FilterParams params = {
             0.02f,
             numParticles,
-            0.005,
+            variance,
             0.99,           // delta
             0.02,           // epslion
             0.1,            // bin size
@@ -17,8 +21,8 @@ int time_experiments(int numParticles, int count) {
     //  File directories to use
     bool save = true;
     std::ofstream timingFile; 
-    std::string guessFileName = "../analysis/experiment-full/res2-" + std::to_string(count) + ".txt"; 
-    std::string timeFileName = "../analysis/experiment-full/res2-timing-" + std::to_string(count) + ".txt"; 
+    std::string guessFileName = "../analysis/experiment-full/runs/" + std::to_string(run) + "-res" + std::to_string(res) + "-" + std::to_string(count) + ".txt"; 
+    std::string timeFileName = "../analysis/experiment-full/runs/" + std::to_string(run) + "-res" + std::to_string(res) + "-timing-" + std::to_string(count) + ".txt"; 
     timingFile.open(timeFileName); 
 
     // Maximum number of frames that are processed
@@ -33,7 +37,7 @@ int time_experiments(int numParticles, int count) {
 
     // Load all frames 
     pcl::PointCloud<RefPointType>::Ptr cloud (new pcl::PointCloud<RefPointType>);
-    std::string fileNames = "../experiments/res2/frame_";
+    std::string fileNames = "../experiments/res" + std::to_string(res) +"/frame_";
     std::chrono::high_resolution_clock::time_point startTime, endTime;
 
     while (tracker.frameCount < maxFrames) {
@@ -101,15 +105,29 @@ int run_experiment(double variance, int numParticles, float downSampleLevel, int
 int batchExperiment() {
     std::vector<double> variance = { 0.001, 0.005, 0.05, 0.08 };
     std::vector<int> numParticles = { 500, 1000, 2000, 3000 };
-    std::vector<int> partCount = { 100, 600 ,1000, 1500, 2000 }; 
-    // 100, 600 ,1000, 1500, 2000 
+    std::vector<int> partCount = { 100, 600 ,1000, 1500, 2000 }; // 100, 600 ,1000, 1500, 2000 
     std::vector<float> downSampleLevel = { 0.01f, 0.02f, 0.06f, 0.1f };
-    int count = 1;
+    // int count = 1;
 
-    for (int n : partCount) { 
-        time_experiments(n, count); 
-        std::cout << "Completed experiment " << count << " / 5" << std::endl; 
-        count++;
+    // for (int n : partCount) { 
+    //     time_experiments(n, count); 
+    //     std::cout << "Completed experiment " << count << " / 5" << std::endl; 
+    //     count++;
+    // }
+
+    // For all of the possible camera resolutions
+    for (int r = 3; r <= 5; r++) {
+        for (int i = 1; i <= 5; i++) {
+            int count = 1; 
+            // Analyse the time and accuracy given a specific particle count
+            for (int n : partCount) {
+                // Run 1 
+                // time_experiments(int numParticles, int res, int count, int run) 
+                time_experiments(n, i, count, r); 
+                std::cout << "Resolution " << i << ", completed " << count << " / 5" << std::endl; 
+                count++;
+            }
+        }
     }
 
     // for (double v : variance) {
